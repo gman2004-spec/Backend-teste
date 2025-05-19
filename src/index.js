4dfa772a93e2cd5ef548929b2e2c7d0d0bdf55b7
gman2004-spec <Viniciusbragastrujak@gmail.com>
2025-05-19 16:36:38 -0300

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import http from 'http'
+import app from './app.js'
+
+const port = 5555
+
+const server = http.createServer(app)
+
+server.listen(port, () => {
+    return console.log(`Escutando na porta ${port}`)
+})
