ca73c023a61eb047d65798fb313fa7bc8f8d8d3f
gman2004-spec <Viniciusbragastrujak@gmail.com>
2025-05-21 16:15:10 -0300

    Update routes.ts

--- src/routes.ts
+++ src/routes.ts
@@ -65,5 +65,7 @@ routes.get("/withImages", (req: Request, res: Response) : any => {
 routes.post("/users", (req: Request, res: Response) : any => userController.create(req, res))
 routes.get("/users", (req: Request, res: Response) : any => userController.read(req, res))
 routes.put("/users/id", (req: Request, res: Response) : any => userController.update(req, res)) 
+routes.delete("/users/:id", (req: Request, res: Response) : any => userController.delete(req, res))
+routes.post("/login", (req: Request, res: Response) : any => userController.login(req, res))
 
 export default routes
